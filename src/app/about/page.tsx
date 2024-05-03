265b24538151981743a382f9bc3378db896e26ec
RichardKrijnseLocker <richard@notmedior.nl>
2024-05-03 11:08:07 +0200

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,9 @@
 export default function AboutPage() {
 
     return (
-        <h1>about</h1>
+        <main
+            className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+            <h1>about</h1>
+        </main>
     )
 }
