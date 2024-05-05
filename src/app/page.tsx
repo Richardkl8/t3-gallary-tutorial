20875f0919f74a616c2a9021af2686c2d1691ca5
RichardKrijnseLocker <richard@notmedior.nl>
2024-05-05 14:01:57 +0200

    next image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,40 @@
-import {db} from "~/server/db";
+import {getUserImages} from "~/server/queries";
+import {SignedIn, SignedOut} from "@clerk/nextjs";
+import Image from "next/image";
 
 export const dynamic = 'force-dynamic'
 
+async function Images() {
+  const images = await getUserImages();
+
+  return (
+    <div className={'flex flex-wrap justify-center flex-row gap-4'}>
+      {images.map(({url, name}, index) => (
+        <div key={name + index} className={'flex flex-col'}>
+          <div className={'w-96 h-96'}>
+            <Image
+              style={{objectFit: 'cover', height: '100%', width: '100%'}}
+              height={384}
+              width={384}
+              src={url}
+              alt={'auto'}
+            />
+          </div>
+          <p className={'text-center'}>{name}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
 export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, {desc}) => desc(model.id)
-  })
+
 
   return (
     <main className="min-h-screen">
-      <div className={'flex flex-wrap gap-4'}>
-
-        {images.map(({url, name}, index) => (
-          <div key={name+index}>
-            <img className={'object-cover w-96 h-96'}
-             src={url} alt={'sdf'}/>
-            <p className={'w-96 text-xl'}>{name}</p>
-          </div>))}
-      </div>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
     </main>
   );
 }
